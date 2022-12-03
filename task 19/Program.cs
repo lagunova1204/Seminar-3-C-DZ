6311d8f68a2a60764b06f5fc29f09ac9f7188ec2
lagunova1204 <lagunova.irina1204@gmail.com>
2022-12-03 15:03:48 +0300

    Задача 19

---
+++ task 19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a/10000) == (a%10))
+{ 
+    a = (a/10) % 1000;
+    if ((a/100)==(a%10))
+    System.Console.WriteLine($"Число {a} - полиндром");
+    else
+    {
+        System.Console.WriteLine($"Число {a} - не полиндром");
+    }
+}
+else
+{
+    System.Console.WriteLine($"Число {a} - не полиндром");
+}
