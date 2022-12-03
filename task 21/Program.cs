a1b37e72174bf05480b2faf2854cc32490b3f905
lagunova1204 <lagunova.irina1204@gmail.com>
2022-12-03 15:21:13 +0300

    Задача 21

---
+++ task 21/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите координаты х1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты х1");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double dist = Math.Sqrt(Math.Pow((x2-x1), 2) + (Math.Pow((y2-y1), 2)) + (Math.Pow((z2-z1), 2)));
+Console.WriteLine($"Расстояние между двумя точками {Math.Round(dist, 1)}");
